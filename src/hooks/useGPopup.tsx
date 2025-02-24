16f6ba579301978a8824f2d4a34050b0b9237fc3
sundaresanRedsco <135582374+sundaresanRedsco@users.noreply.github.com>
2025-02-24 13:58:00 +0530

    minor fix

--- src/hooks/useGPopup.tsx
+++ src/hooks/useGPopup.tsx
@@ -1,31 +1,31 @@
-// "use client";
-// import React, { useState } from "react";
+"use client";
+import { useSignUpStore } from "@/store/useSignUpStore";
+import React, { useState } from "react";
 // import GPopup from "@/app/apiflow_components/global/GPopup";
-// import { useSignUpStore } from "./sign/signZustand";
 
-// export default function useGPopup() {
-//   const { open, setopen, handleOpen, message } = useSignUpStore();
-//   const handleClose = () => {
-//     setopen(false);
-//   };
+export default function useGPopup() {
+  const { open, setopen, handleOpen, message } = useSignUpStore();
+  const handleClose = () => {
+    setopen(false);
+  };
 
-//   const PopUpComponent = ({
-//     width,
-//     height,
-//   }: {
-//     width?: string;
-//     height?: string;
-//   }) => {
-//     return (
-//       <GPopup
-//         height={height}
-//         width={width}
-//         isOpen={open}
-//         handleClose={handleClose}
-//         message={message}
-//       />
-//     );
-//   };
-//   return { PopUpComponent, handleClose, handleOpen, open };
-// }
-export {};
+  const PopUpComponent = ({
+    width,
+    height,
+  }: {
+    width?: string;
+    height?: string;
+  }) => {
+    return (
+      //   <GPopup
+      //     height={height}
+      //     width={width}
+      //     isOpen={open}
+      //     handleClose={handleClose}
+      //     message={message}
+      //   />
+      <></>
+    );
+  };
+  return { PopUpComponent, handleClose, handleOpen, open };
+}
