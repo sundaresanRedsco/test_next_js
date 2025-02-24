ca1868fce0d5961c787efa3d0c7116e8ffa7012b
sundaresanRedsco <135582374+sundaresanRedsco@users.noreply.github.com>
2025-02-24 16:04:27 +0530

    removed code

--- src/app/onboarding/email-verification/page.tsx
+++
@@ -1,8 +0,0 @@
-import EmailVerification from "@/pages/onboarding/EmailVerification";
-import React from "react";
-
-type Props = {};
-
-export default function OnboardingPage({}: Props) {
-  return <EmailVerification />;
-}
--- src/app/onboarding/page.tsx
+++
@@ -1,9 +0,0 @@
-import Onboarding from "@/components/onboarding";
-import WelcomePage from "@/pages/onboarding/WelcomePage";
-import React from "react";
-
-type Props = {};
-
-export default function OnboardingPage({}: Props) {
-  return <WelcomePage />;
-}
